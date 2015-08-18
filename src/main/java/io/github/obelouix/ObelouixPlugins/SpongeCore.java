62533c0c147c1d120354d5eac5b1253cd2a9f8fd
Louis <obelouix25@gmail.com>
2015-08-18 15:10:53 +0200

    pas besoin d'ajouter le fichier "config.conf" Sponge le créer

--- src/main/java/io/github/obelouix/ObelouixPlugins/SpongeCore.java
+++ src/main/java/io/github/obelouix/ObelouixPlugins/SpongeCore.java
@@ -1,13 +1,20 @@
 package io.github.obelouix.ObelouixPlugins;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.bukkit.Server.Spigot;
 import org.slf4j.Logger;
 import org.spongepowered.api.event.Subscribe;
 import org.spongepowered.api.event.state.ServerAboutToStartEvent;
 import org.spongepowered.api.event.state.ServerStartedEvent;
 import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.service.config.DefaultConfig;
 
 import com.google.inject.Inject;
+
+import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+import ninja.leaping.configurate.loader.ConfigurationLoader;
 
 @Plugin(id = "ObelouixEss", name = "ObelouixEssentials", version = "1.0")
 public class SpongeCore {
@@ -15,17 +22,40 @@ public class SpongeCore {
 	@Inject
 	private Logger logger;
 	
+	@Inject
+	@DefaultConfig(sharedRoot = true)
+	private File defaultConfig;
+
+	@Inject
+	@DefaultConfig(sharedRoot = true)
+	private ConfigurationLoader<CommentedConfigurationNode> configManager;
+	
 	@Subscribe
 	public void ServerAboutToStart(ServerAboutToStartEvent e)
 	{
 	  logger.info("Detected Minecraft Server API : Sponge");
+	 
 	}
 	
 	@Subscribe
     public void onServerStart(ServerStartedEvent event) {
-        // Hey! The server has started!
-        // Try instantiating your logger in here.
-        // (There's a guide for that)
+		 CommentedConfigurationNode config = null;
+
+		  try {
+		      if (!defaultConfig.exists()) {
+		          defaultConfig.createNewFile();
+		          config = configManager.load();
+
+		          config.getNode("version").setValue(1);
+		          config.getNode("doStuff").setValue(true);
+		          config.getNode("doMoreStuff").setValue(false);
+		          configManager.save(config);
+		      }
+		      config = configManager.load();
+
+		  } catch (IOException exception) {
+		     logger.error("The default configuration could not be loaded or created!");
+		  }
     }
 
 }
