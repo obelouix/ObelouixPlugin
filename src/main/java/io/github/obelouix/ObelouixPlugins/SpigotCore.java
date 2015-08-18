4ceaaa779e97141b7335d61f7cbec5d0a2fced38
Louis <obelouix25@gmail.com>
2015-08-18 15:06:14 +0200

    Sous Spigot le plugin sauvegarde le fichier config

--- src/main/java/io/github/obelouix/ObelouixPlugins/SpigotCore.java
+++ src/main/java/io/github/obelouix/ObelouixPlugins/SpigotCore.java
@@ -12,6 +12,7 @@ public class SpigotCore extends JavaPlugin {
 	public void onEnable()
 	{
 		console.sendMessage("[ObelouixEss]Detected Minecraft Server API : Spigot");
+		saveDefaultConfig();
 	}
 	
 	@Override
